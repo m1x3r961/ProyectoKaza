78ba8252d9a63ff681ba8bef96612f7b1aecaa16
manitos xpress <142053479+ManitosXpress@users.noreply.github.com>
2026-07-27 20:37:22 -0400

    fix: restore missing flutter/material.dart import

--- mobile/lib/features/saved/screens/saved_screen.dart
+++ mobile/lib/features/saved/screens/saved_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../app/theme/kaza_theme.dart';
 import '../../../core/network/supabase_config.dart';
