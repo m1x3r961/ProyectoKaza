09477e4b3259fe4dc22d48962adf6ec74148141b
manitos xpress <142053479+ManitosXpress@users.noreply.github.com>
2026-07-29 11:11:02 -0400

    fix: remove redundant location section from Step 1 of publish flow

--- mobile/build/web/flutter_bootstrap.js
+++ mobile/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3466164809"
+    serviceWorkerVersion: "3703814336"
   }
 });
