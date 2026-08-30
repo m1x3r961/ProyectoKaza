c74bf4c133cdf45c9095c26a0a80d41548d9580a
manitos xpress <142053479+ManitosXpress@users.noreply.github.com>
2026-08-30 10:48:07 -0400

    design: probar estilo de mapa MapTiler Streets-v2

--- mobile/build/web/flutter_bootstrap.js
+++ mobile/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "134728777"
+    serviceWorkerVersion: "3119136471"
   }
 });
